0464066a626f2d2cd1f4484e78dc7be611a67301
dderaad <dideraad@gmail.com>
2024-07-01 04:10:07 -0400

    Added Loading screen

--- my_app.py
+++ my_app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, no_update
 from mandelbrot import smoothed_mandelbrot
 from utils import *
 import plotly.express as px
@@ -43,9 +43,14 @@ server = app.server
 
 app.layout = [
     html.Div(children=APP_TITLE),
-    dcc.Graph(id='mandelbrot-fig', figure=fig, 
-              style={'wdith': '90vh', 'height': '90vh'}),
-    html.Div(id='zoom-display', children='')
+    dcc.Loading(
+        dcc.Graph(id='mandelbrot-fig', figure=fig, 
+            style={'height': '90vh'}), 
+        type="cube", id="loading-1", 
+        overlay_style={"visibility": "visible", "filter":"blur(2px)"},
+        delay_show=200,
+        ),
+    html.Div(id='zoom-display', children=''),
 ]
 
 @callback(
@@ -59,7 +64,7 @@ def zoom_event(relayout_data, figure):
     # be re-generated for the subset of the plane that is zoomed
     # in on.
     # Similar to https://stackoverflow.com/questions/56611105/how-to-get-zoom-level-in-time-series-data-as-callback-input-in-dash/64891612#64891612
-    
+
     zoomed_figure = figure
     xbounds = 0
     ybounds = 0
@@ -81,4 +86,5 @@ def zoom_event(relayout_data, figure):
 if __name__ == "__main__":
     app.run(host='0.0.0.0', 
             port=int(os.environ.get('PORT', 4000)), 
-            debug=True)
+            #debug=True
+            )
