44a480beab2cb90d095162a2c2377c69d6dca5df
dderaad <dideraad@gmail.com>
2026-04-06 11:11:02 -0700

    Change default port from 4000 to 10000
    Compliance with new render.com defaults(?)

--- my_app.py
+++ my_app.py
@@ -219,5 +219,5 @@ if __name__ == "__main__":
         except Exception as E:
             print(f'The OS {os.name} is unsupported.\n{E}')
         app.run(host='0.0.0.0', 
-                    port=int(os.environ.get('PORT', 4000))
+                    port=int(os.environ.get('PORT', 10000))
                 )
